d793533cd6a514911459aca01fc1f780c9d56491
Ashley Barcel <ashleyemanon@hotmail.com>
2023-02-09 10:59:19 -0500

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   #get "/about"
+
+  resources :inquiries, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_014420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.text "desires"
+    t.text "experience"
+    t.text "explore"
+    t.string "location"
+    t.string "contact_info"
+    t.string "employment"
+    t.text "references"
+    t.text "crimes"
+    t.text "drug_history"
+    t.text "essay"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
